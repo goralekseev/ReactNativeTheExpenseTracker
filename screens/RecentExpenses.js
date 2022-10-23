96f6c7159f0b765ba276fe3b1309ca305e76ac3f
Igor Alekseev <tauron19@gmail.com>
2022-10-23 01:18:31 -0400

    useContext added

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,20 @@
+import { useContext } from 'react';
 import { Text } from 'react-native';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import { ExpensesContext } from '../store/expenses-context';
+import { getDateMinusDays } from '../util/date';
 
 function RecentExpenses() {
-  return <ExpensesOutput expensesPeriod="Last 7 days" />;
+  const expensesCtx = useContext(ExpensesContext);
+
+  const RecentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date > date7DaysAgo;
+  });
+  return (
+    <ExpensesOutput expenses={RecentExpenses} expensesPeriod="Last 7 days" />
+  );
 }
 export default RecentExpenses;
